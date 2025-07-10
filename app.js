1046a2165365616cdf3c60c7f0c8d800867f6fb3
Daniil Baida <devbaida78@gmail.com>
2025-07-10 19:59:47 +0100

    basic routes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express, { response } from "express";
 
 import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
 
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 app.get("/", (req, res) => {
-    response.send("Welcome to the Subscription Tracker API");
+    res.send("Welcome to the Subscription Tracker API");
 });
 
 app.listen(PORT, () => {
